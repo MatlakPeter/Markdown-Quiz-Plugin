85b86b2fd2cdd83a698fbb4bf4a2a0b5aae452a0
Filip <filip.pintea@gmail.com>
2026-01-13 20:37:03 +0200

    updated setup to display a newer version + published new mkdocs version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-quiz_plugin',
-    version='0.1.0',
+    version='0.9.0',
     description='A simple MkDocs plugin to insert a custom HTML component.',
     author='Your Name',
     packages=find_packages(),
